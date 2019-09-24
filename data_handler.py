d0ae974f6841c7acf535f3a62231d63ebcf6774b
Lorya <mircsi.lodi@gmail.com>
2019-09-24 13:46:58 +0200

    Change data_handler.py to use SQL

--- data_handler.py
+++ data_handler.py
@@ -1,30 +1,33 @@
-import persistence
+import database_common
 
 
-def get_card_status(status_id):
-    """
-    Find the first status matching the given id
-    :param status_id:
-    :return: str
-    """
-    statuses = persistence.get_statuses()
-    return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
+@database_common.connection_handler
+def get_card_status(cursor, status_id):
+    cursor.execute("""
+                    SELECT title 
+                    FROM statuses
+                    WHERE id = $(status_id)s;
+                    """,
+                   {'status_id': status_id})
+    card_status = cursor.fetchone()
+    return card_status
 
 
-def get_boards():
-    """
-    Gather all boards
-    :return:
-    """
-    return persistence.get_boards(force=True)
+@database_common.connection_handler
+def get_boards(cursor):
+    cursor.execute("""
+                    SELECT * FROM boards;
+                    """)
+    boards = cursor.fetchall()
+    return boards
 
 
-def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
-    matching_cards = []
-    for card in all_cards:
-        if card['board_id'] == str(board_id):
-            card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-            matching_cards.append(card)
-    return matching_cards
+@database_common.connection_handler
+def get_cards_for_board(cursor, board_id):
+    cursor.execute("""
+                    SELECT * FROM cards
+                    WHERE board_id = $(board_id)s;
+                    """,
+                   {'board_id': board_id})
+    cards_for_board = cursor.fetchall()
+    return cards_for_board
